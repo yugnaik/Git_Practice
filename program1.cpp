1263bfac4976c9e5770595b8ece2b23cd01eb228
Yug Santosh Naik <yugnaik27@gmail.com>
2024-12-28 22:33:53 +0530

    Updated program1.cpp with a sum function

--- program1.cpp
+++ program1.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
-
+int sum (int a , int b){
+    return a+b;
+}
 int main() {
     cout << "Hello World"<< endl;
+    cout<< "Sum:"<<sum(5,7)<< endl;
     return 0;
 
 }
